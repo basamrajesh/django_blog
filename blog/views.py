e10f05996f1ccb1858000aada62ec88b380a6df2
Basam Rajesh Chowdary <basamrajeshchowdary@gmail.com>
2019-06-06 22:27:54 +0530

    First commit of CoreyBlog

---
+++ blog/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+books = [
+    {
+        'title': "My First Book",
+        'author': "Basam",
+        'snippet': "Abaha em rasav raa nayana book bhale undi"
+    },
+    {
+        'title': "My Second Book",
+        'author': "Basam Rajesh",
+        'snippet': "Abaha em rasav raa nayana book bhale undi ani anta anukunnav kada"
+    },
+    {
+        'title': "My Third Book",
+        'author': "Basam Rajesh",
+        'snippet': "Abaha em rasav raa nayana book bhale undi ani anta anukunnav kada"
+    }
+]
+
+
+def home(request):
+    # context = {
+    #     'books': books
+    # }
+    return render(request, 'blog/home.html', context={'books': books})
